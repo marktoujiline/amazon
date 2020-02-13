d807b212b1602d0798e624d5e9f6cd41b7e3642f
Cynthia Santee <santeecynthia@gmail.com>
2020-02-13 15:51:44 -0800

    add redux, storybook and test redux

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import counter from "./redux/counter";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  counter: counter.reducer
+});
+const store = configureStore({ reducer: rootReducer });
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
