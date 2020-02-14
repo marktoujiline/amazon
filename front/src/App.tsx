f73acea54b219d94448a023e1c9f5bc0fcb13c82
Cynthia Santee <santeecynthia@gmail.com>
2020-02-13 21:05:19 -0800

    add navigation

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,61 @@
 import React from "react";
-import Counter from "./react/Counter";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Home = () => {
+  return <h2>Home</h2>;
+};
+
+const Cart = () => {
+  return <h2>Cart</h2>;
+};
+
+const Search = () => {
+  return <h2>Search</h2>;
+};
+
+const Account = () => {
+  return <h2>Account</h2>;
+};
 
 const App = () => {
   return (
     <div className="App">
-      <Counter></Counter>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/account">Account</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/account">
+              <Account />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
