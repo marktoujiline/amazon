de1fe52c3d85efa62e5774140c7dd4993a4cd90d
Cynthia Santee <santeecynthia@gmail.com>
2020-02-13 19:43:03 -0800

    removed unused files

--- front/src/stories/Counter.stories.js
+++ front/src/stories/Counter.stories.js
@@ -1,18 +1,5 @@
 import React from "react";
 import { Counter } from "../react/Counter";
-// import { linkTo } from "@storybook/addon-links";
-// import { Welcome } from "@storybook/react/demo";
-
-// export default {
-//   title: "Welcome",
-//   component: Welcome
-// };
-
-// export const ToStorybook = () => <Welcome showApp={linkTo("Button")} />;
-
-// ToStorybook.story = {
-//   name: "to Storybook"
-// };
 
 export default {
   component: Counter,
